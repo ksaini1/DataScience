af5eb08e370ecb79ee53ed4bbb44a64f2b4a20b1
ksaini1 <47371717+ksaini1@users.noreply.github.com>
2019-10-25 13:43:03 -0400

    mongo

--- code2.py
+++ code2.py
@@ -2,6 +2,10 @@ import tweepy
 from tweepy import Cursor
 import unicodecsv
 from unidecode import unidecode
+from pymongo import MongoClient
+import json
+from pymongo import Connection
+import datetime
 
 ####input your credentials here
 consumer_key = 'VWumRz5dQcduRj9FQel4f8qcm'
@@ -9,13 +13,18 @@ consumer_secret = 'EfXmu5vfFd00t3BfX42JbsaNOf3LCC3Is4je5hQfpGuCmvhjDX'
 access_token = '965507672945713152-bZzrscE41RQWctVkm86uGuAv13oo1uB'
 access_token_secret = 'PGvvgnXM5omK0tTQZdtUi3KSLjs6MnjXZx09IVQgqiSST'
 
+connection = Connection('127.0.0.1', 27017)
+db = connection.TwitterStream
+db.tweets.ensure_index("id", unique=True, dropDups=True)
+collection = db.tweets
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 #####United Airlines
 # Open/Create a file to append data
 #with open('tweetstry.csv') as file:
-with open('tweetstry.csv', 'wb') as file:
+with open('tweets.csv', 'wb') as file:
     #writer = unicodecsv.writer(file)
     writer = unicodecsv.writer(file, delimiter = ',', quotechar = '"')
     # Write header row.
